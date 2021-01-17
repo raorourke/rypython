d944a99b6c67dfc644e660e6a5bb2aacaddfe41b
welocalize <aie-devel@welocalize.com>
2021-01-17 09:00:29 -0500

    push to rypy 0.1.1

---
+++ rypy/find/__init__.py
@@ -0,0 +1,14 @@
+import re
+
+
+def rexex(pattern: str, query: str):
+    r = re.compile(pattern)
+    s = [
+        m.groupdict()
+        for m in r.finditer(query)
+    ]
+    d = {}
+    for match in s:
+        for key, value in match.items():
+            d.setdefault(key, []).append(value)
+    return d
