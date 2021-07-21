99fe6f7e61d0f34ed8fd635f44ee76fb3972131a
Ryan O'Rourke <ryan.orourke@welocalize.com>
2021-07-20 22:03:11 -0400

    1.0.1

--- rypython/ry365/__init__.py
+++ rypython/ry365/__init__.py
@@ -1,3 +1,3 @@
-from ry365.account import O365Account
-from ry365.excel import WorkBook, WorkSheet, Range
-from ry365.drive import Folder, Drive, Storage
+from rypython.ry365.account import O365Account
+from rypython.ry365.excel import WorkBook, WorkSheet, Range
+from rypython.ry365.drive import Folder, Drive, Storage
