f7da23001b3b835283105f25f0f4257743a88500
Ryan O'Rourke <ryan.orourke@welocalize.com>
2022-02-22 11:55:51 -0500

    Add support for conditional fomatting in RexcelWorkbook class

--- rypython/rexcel/__init__.py
+++ rypython/rexcel/__init__.py
@@ -23,6 +23,9 @@ class RexcelWorkbook:
     def __exit__(self, type, value, traceback):
         self.workbook.close()
 
+    def add_format(self, config: dict):
+        return self.workbook.add_format(config)
+
     def add_worksheet_by_dataframe(
             self,
             df: pd.DataFrame,
@@ -30,7 +33,8 @@ class RexcelWorkbook:
             column_widths: list,
             include_index: bool = False,
             format_rows: Tuple[Callable, Format] = None,
-            formula_columns: dict = None
+            formula_columns: dict = None,
+            conditional_formatting: dict = None
     ):
         df = df.where(pd.notnull(df), '')
         format_test, row_format = format_rows if format_rows else (None, None)
@@ -89,4 +93,12 @@ class RexcelWorkbook:
                         wks.write_formula(*cell_info)
                     offset += 1
             row_number += 1
+        if conditional_formatting:
+            for format_range, config in conditional_formatting.items():
+                if '{end}' in format_range:
+                    format_range = format_range.format(end=len(rows) + 1)
+                wks.conditional_format(
+                    format_range,
+                    config
+                )
         self.worksheets.append(wks)
