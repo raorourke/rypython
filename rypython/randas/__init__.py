94f531e0937c0b9fb55f02a2973e89368bc3aa3b
Ryan O'Rourke <ryan.orourke@welocalize.com>
2022-03-02 18:02:22 -0500

    Update read_excel365

--- rypython/randas/__init__.py
+++ rypython/randas/__init__.py
@@ -4,10 +4,13 @@ from O365.drive import File
 from rypython.ry365 import WorkBook
 
 
-def read_excel365(xl_file: File, skip_rows: int = 0):
+def read_excel365(xl_file: File, sheet_name: str = None, skip_rows: int = 0):
     wb = WorkBook(xl_file)
-    wss = wb.get_worksheets()
-    ws = wss[0]
+    if sheet_name:
+        ws = wb.get_worksheet(sheet_name)
+    else:
+        wss = wb.get_worksheets()
+        ws = wss[0]
     _range = ws.get_used_range()
     cols, *values = _range.values[skip_rows:]
     df = pd.DataFrame(values, columns=cols)
