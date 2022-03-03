39168d8aaf7aabd1d505a3c8a8f8174c7e281fc8
Ryan O'Rourke <ryan@rypy.dev>
2022-03-03 13:58:07 -0500

    Add support for reading all sheets from O365 file

--- rypython/randas/__init__.py
+++ rypython/randas/__init__.py
@@ -1,14 +1,21 @@
 import pandas as pd
 from O365.drive import File
+from typing import Union
 
 from rypython.ry365 import WorkBook
 
 
-def read_excel365(xl_file: File, sheet_name: str = None, skip_rows: int = 0):
-    wb = WorkBook(xl_file)
-    if sheet_name:
+def read_excel365(xl_file: Union[File, WorkBook], sheet_name: str = False, skip_rows: int = 0):
+    wb = WorkBook(xl_file) if isinstance(xl_file, File) else xl_file
+    if sheet_name is None:
+        wss = wb.get_worksheets()
+        dfs = {}
+        for ws in wss:
+            dfs[ws.name] = read_excel365(wb, sheet_name=ws.name, skip_rows=skip_rows)
+        return dfs
+    if sheet_name is False:
         ws = wb.get_worksheet(sheet_name)
-    else:
+    elif sheet_name:
         wss = wb.get_worksheets()
         ws = wss[0]
     _range = ws.get_used_range()
@@ -16,3 +23,6 @@ def read_excel365(xl_file: File, sheet_name: str = None, skip_rows: int = 0):
     df = pd.DataFrame(values, columns=cols)
     df.range = _range
     return df
+
+
+
