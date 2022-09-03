a9831b884da26cb531d8089d949f064df58e4e8e
Ryan O'Rourke <ryan@rypy.dev>
2022-09-03 11:11:32 -0400

    [feature] Add initial formats for RexcelWorkbook

---
+++ rypython/rexcel/formats.py
@@ -0,0 +1,16 @@
+RED = {
+    'bg_color': '#FFC7CE',
+    'font_color': '#9C0006'
+}
+
+YELLOW = {
+    'bg_color': '#FFEB9C',
+    'font_color': '#9C6500'
+}
+
+GREEN = {
+    'bg_color': '#C6EFCE',
+    'font_color': '#006100'
+}
+
+EMPTY_CELL = ""
